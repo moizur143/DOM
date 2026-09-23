4e318e0364be53bdbb2ef9d8d8b8142603704741
mohammad moiz akmal <moizakmal46@gmail.com>
2026-09-23 13:27:43 +0530

    dobule tap like

--- script.js
+++ script.js
@@ -1,30 +1,22 @@
-let btn =document.querySelector('button');
-let grow = 0;
-let inner = document.querySelector('.inner')
-let h2 =document.querySelector('h2')
+var img =document.querySelector('img');
+var i =document.querySelector('i');
 
-
-btn.addEventListener('click',function(){
-    btn.style.pointerEvents ='none'
-
-    let num = 50 + Math.floor(Math.random()*50)
-    console.log(num)
-
-
-    let int = setInterval(function(){
-        grow++
-        h2.innerHTML = grow+'%'
-        inner.style.width = grow+'%'
-    },num)
+img.addEventListener('dblclick',function(){
+    i.style.opacity = 1
+    i.style.transform = 'translate(-50% , -50%) scale(1) rotate(0deg)'
 
     setTimeout(function(){
-    clearInterval(int);
-    btn.innerHTML ='Downloded';
-    btn.style.opacity = 0.5
-    console.log(`Downloded in` ,num/10, `second` )
-},num*100)
+        i.style.transform = 'translate(-50% , -300%) scale(1) rotate(60deg)'
 
+    },800)
 
+    setTimeout(function(){
+        i.style.opacity = 0 
+    },1000)
+
+    setTimeout(function(){
+        i.style.transform = 'translate(-50% , -50%) scale(0) rotate(60deg)'
+    },1200)
 })
 
 
